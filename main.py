21e24b1a51a6fb23bdd5bcc1738b4748e1fea899
Jonathan Moallem <jdmoallem@gmail.com>
2019-05-25 14:44:44 +1000

    Generated testing results for decision tree classification

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ df_test = pd.read_csv('banking_testing.csv')
 # Set up features and targets for prediction
 feature_cols = ['age', 'duration', 'campaign', 'pdays', 'previous']
 target_col = 'Final_Y'
+
+# Build a decision tree from training data
+dt = decision_tree.build_tree(df_train[feature_cols], df_train[target_col])
+
+# Create a combined data frame for output of predictions
 output_df = pd.DataFrame({
-    'row ID': df_train['row ID'],
-    'Final_Y': df_train['Final_Y'],
+    'row ID': df_test['row ID'],
+    'Final_Y': dt.predict(df_test[feature_cols]),
 })
 
 # Write the data frame to a csv file
-output_df.to_csv('output.csv', index=False)
+output_df.to_csv('fda_a3_12582589.csv', index=False)
