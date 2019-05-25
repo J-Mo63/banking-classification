11998385a1d5123c0e5d7a4e8d0ac305ebc47780
Jonathan Moallem <jdmoallem@gmail.com>
2019-05-25 14:44:39 +1000

    Set target and feature columns

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Import libraries
+from models import decision_tree
 import pandas as pd
 
 # Import the data and read from csv file
 df_train = pd.read_csv('banking_training.csv')
 df_test = pd.read_csv('banking_testing.csv')
 
-# Create a combined data frame for output
+# Set up features and targets for prediction
+feature_cols = ['age', 'duration', 'campaign', 'pdays', 'previous']
+target_col = 'Final_Y'
 output_df = pd.DataFrame({
     'row ID': df_train['row ID'],
     'Final_Y': df_train['Final_Y'],
