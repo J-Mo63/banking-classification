f94b565d97a225de9fc2539b8d118c424d185e16
Jonathan Moallem <jdmoallem@gmail.com>
2019-05-26 11:07:35 +1000

    Made decision tree model export a visualisation of the tree on run

--- models/decision_tree.py
+++ models/decision_tree.py
@@ -2,6 +2,10 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.tree import export_graphviz
+from io import StringIO
+from IPython.display import Image
+import pydotplus
 
 
 def build_tree(df_train, df_target):
@@ -18,6 +22,15 @@ def build_tree(df_train, df_target):
     # Determine accuracy for testing targets and notify console
     accuracy = metrics.accuracy_score(test_targets, predictions)
     print("Decision Tree Accuracy: " + "{0:.3%}".format(accuracy))
+    # Visualise the decision tree and export to png
+    dot_data = StringIO()
+    export_graphviz(dt, out_file=dot_data,
+                    filled=True, rounded=True,
+                    special_characters=True, feature_names=list(train_features),
+                    class_names=['no', 'yes'])
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png('decision_tree.png')
+    Image(graph.create_png())
 
     # Return the generated decision tree
     return dt
