7dfbca2f8e3cbb966a252c8bce0f4486376c9072
Jonathan Moallem <jdmoallem@gmail.com>
2019-06-04 20:24:49 +1000

    Made neural network predict from banking data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Import libraries
-from models import decision_tree, support_vector_machine
+from models import decision_tree, support_vector_machine, neural_network
 from utils import pre_processing as prep
 import pandas as pd
 import numpy as np
@@ -12,16 +12,16 @@ df_test = pd.read_csv('banking_testing.csv')
 # Set up features and targets for prediction
 target_col = 'Final_Y'
 
-#Process the data sets for use
-processed_train = decision_tree.process_data(df_train, target_col, include_target=True)
-processed_test = decision_tree.process_data(df_test, target_col)
-
-# processed_train = processed_train[(np.abs(stats.zscore(processed_train)) < 3).all(axis=1)]
-
-# Build a decision tree from training data
-clf = decision_tree.build_dt(
-    prep.remove_target(processed_train, target_col),
-    processed_train[target_col])
+# #Process the data sets for use
+# processed_train = decision_tree.process_data(df_train, target_col, include_target=True)
+# processed_test = decision_tree.process_data(df_test, target_col)
+#
+# # processed_train = processed_train[(np.abs(stats.zscore(processed_train)) < 3).all(axis=1)]
+#
+# # Build a decision tree from training data
+# clf = decision_tree.build_dt(
+#     prep.remove_target(processed_train, target_col),
+#     processed_train[target_col])
 
 
 # # Process the data sets for use
@@ -33,11 +33,19 @@ clf = decision_tree.build_dt(
 #     prep.remove_target(processed_train, target_col),
 #     processed_train[target_col])
 
-# Create a combined data frame for output of predictions
-output_df = pd.DataFrame({
-    'row ID': df_test['row ID'],
-    'Final_Y': clf.predict(processed_test)
-})
+#Process the data sets for use
+processed_train = neural_network.process_data(df_train, target_col, include_target=True)
+processed_test = neural_network.process_data(df_test, target_col)
 
-# Write the data frame to a csv file
-output_df.to_csv('fda_a3_12582589.csv', index=False)
+clf = neural_network.build_nn(
+    prep.remove_target(processed_train, target_col),
+    processed_train[target_col])
+
+# Create a combined data frame for output of predictions
+# output_df = pd.DataFrame({
+#     'row ID': df_test['row ID'],
+#     'Final_Y': clf.predict(processed_test)
+# })
+#
+# # Write the data frame to a csv file
+# output_df.to_csv('fda_a3_12582589.csv', index=False)
